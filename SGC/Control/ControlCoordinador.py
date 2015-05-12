b132fd444f6bb99b5725d0dd08dfa76b6970759c
brayanrodbajo <bryan951112@gmail.com>
2015-05-12 17:55:31 -0500

    ControlCoordinador.py con crearCurso probado y modificarCurso aun sin probar

--- SGC/Control/ControlCoordinador.py
+++ SGC/Control/ControlCoordinador.py
@@ -1,22 +1,39 @@
 __author__ = 'family'
 
 from ORM.Curso import Curso
+from ORM.Actividades import Actividades
+from Modelo.LogicaCursos import LogicaCursos
 
 class ControlCoordinador:
 
     def __init__(self):
         #self.logCurso = LogicaCurso()
-        return None
+        self.logicaCursos = LogicaCursos()
 
-    def crearCurso(self, nombre, descripcion):
-        curso = Curso(nombre, descripcion)
+    def crearCurso(self, nombre, descripcion, actividades):
+        obj_actividades = []
+        for actividad in actividades:
+            nombre_ac = actividad[0]
+            ponderado_ac = actividad[1]
+            obj_actividad = Actividades(nombre = nombre_ac, ponderado = ponderado_ac)
+            obj_actividades.append(obj_actividad)
+        curso = Curso(nombre= nombre, descripcion=descripcion, actividades = obj_actividades)
+        self.logicaCursos.agregarCurso(curso)
 
-    #si tipo es 1 el criterio es el codigo del curso, si es 2 el criterio es el nombre
-    def consultarCurso(self, criterio, tipo):
-        respuesta = ["nombre", "descripcion"]
-        return respuesta
+    def modificarCurso(self, nombre_c, descripcion_c, actividades):
+        curso = Curso(nombre= nombre_c, descripcion=str(self.ui.teDescripcion.toPlainText()))
+        self.logicaCursos.modificarCurso(nombre_c, curso)
+        i=0
+        curso = self.logicaCursos.consultarCurso(nombre_c)
+        self.logicaActividades.eliminarActividadesXCurso(curso.id)
+        while i<self.ui.sbNumActividades.value():
+            nombre_ac = str(self.ui.twActividades.item(i, 0).text())
+            ponderado_ac = float(self.ui.twActividades.item(i, 1).text())
+            actividad = Actividades(nombre = nombre_ac, ponderado = ponderado_ac, id_curso =curso.id )
+            self.logicaActividades.agregarActividades(actividad)
+            i+=1
 
-    #si tipo es 1 el criterio es el codigo del curso, si es 2 el criterio es el nombre
-    def modificarCurso(self, criterio, tipo, descripcion):
-        #modificacion de descripcion
-        pass
+
+con =  ControlCoordinador()
+actividades = [["act1", 0.2], ["act2", 0.5]]
+con.crearCurso("curso 1", "descripcion curso 1", actividades)
