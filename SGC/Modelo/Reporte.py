8231587ba14edb1dd21890a541d7b834b0748b6d
BRAYAN MAURICIO RODRÍGUEZ RIVERA <braymrr@s3pc2.eisc.univalle.edu.co>
2015-05-13 12:00:32 -0500

    empiezo Reporte.py

--- SGC/Modelo/Reporte.py
+++ SGC/Modelo/Reporte.py
@@ -1,4 +1,10 @@
 __author__ = 'braymrr'
+from ORM.Curso import Curso
+from Modelo.LogicaCursos import LogicaCursos
 
 class Reporte:
-    pass
+    def cursos_mas_asistentes:
+		#se mira fecha de inicio y fin de cohorte de cada curso
+		#si el mes actual pertenece a este rango de fechas 
+		#se cuenta el numero de estudiantes que asistieron a cada nota
+		#
