d2e01cf64cf16270a008ae6cfd75188d0d61ef46
brayanrodbajo <bryan951112@gmail.com>
2015-05-13 18:05:45 -0500

    Primer reporte, en palabras hecho.

--- SGC/Modelo/Reporte.py
+++ SGC/Modelo/Reporte.py
@@ -4,7 +4,8 @@ from Modelo.LogicaCursos import LogicaCursos
 
 class Reporte:
     def cursos_mas_asistentes:
-		#se mira fecha de inicio y fin de cohorte de cada curso
-		#si el mes actual pertenece a este rango de fechas 
-		#se cuenta el numero de estudiantes que asistieron a cada nota
-		#
+        #filtro fecha de inicio y fin de cohorte de cada curso pertenece la fecha actual
+        #agrupo por id_curso
+		#se cuenta en numero de matriculados de cada curso
+        #ordena de menor a mayor y se traen los 10 primeros
+
