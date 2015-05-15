92e62497b0a10c841c6dc4a2a0a7d890efefe04b
brayanrodbajo <bryan951112@gmail.com>
2015-05-15 10:54:03 -0500

    arreglando matricula para llave foranea .. pre-merge fernando

--- SGC/ORM/Matricula.py
+++ SGC/ORM/Matricula.py
@@ -13,13 +13,17 @@ from basetest import *
 class Matricula (Base):
 	
 	__tablename__ = 'matricula'
-	cedula_lt = Column(String(40), primary_key=True)
-	id_curso = Column(Integer,primary_key=True)
-	id_cohorte = Column(Integer, primary_key=True) 
-	ForeignKeyConstraint(['cedula_lt', 'id_curso', 'id_cohorte'],['leaderteacher.cedula','cohorte.id_curso','cohorte.id_cohorte'])
+	cedula_lt = Column(String(40),ForeignKey("leaderteacher.cedula"),  primary_key=True)
+	id_curso = Column(Integer, ForeignKey("cohorte.id_curso"), primary_key=True )
+	id_cohorte = Column(Integer,ForeignKey("cohorte.id_cohorte"), primary_key=True)
 	nota_definitiva = Column(Float)
-	
 
 
 
 Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+mat = Matricula(cedula_lt = 1144123, id_curso=78, id_cohorte = 654)
+session.add(mat)
+session.commit()
