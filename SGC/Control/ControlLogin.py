52e8d83b879139fbe6c21a10bff172a42c1bf933
juandprado <juandipra@hotmail.com>
2015-05-17 22:51:17 -0500

    Anadida version inicial de ControlLogin

---
+++ SGC/Control/ControlLogin.py
@@ -0,0 +1,13 @@
+__author__ = 'JuanD'
+
+from ORM.Usuario import Usuario
+from Modelo.LogicaUsuario import LogicaUsuario
+
+
+class ControlLogin:
+    def __init__(self):
+        # self.logCurso = LogicaCurso()
+        self.logicaUsuario = LogicaUsuario()
+
+    def buscarUsuarioUsername(self, username):
+        return self.logicaUsuario.buscarUsuarioUsername(username)
