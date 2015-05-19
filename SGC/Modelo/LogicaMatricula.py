81faa3eb63b9487175079ccfa699e289ba1b35df
juandprado <juandipra@hotmail.com>
2015-05-18 23:54:59 -0500

    Modificado metodo consutar_cursos_estudiantes

--- SGC/Modelo/LogicaMatricula.py
+++ SGC/Modelo/LogicaMatricula.py
@@ -47,9 +47,10 @@ class LogicaMatricula():
         self.session.close()
         return cantidad
 
-    def consultar_cursos_estudiantes (self,cedula_lt):
+    def consultar_cursos_estudiantes(self, cedula_lt):
         registros = self.session.query(Matricula).filter_by(cedula_lt = cedula_lt).all()
         self.session.close()
+        return registros
         
     
         
