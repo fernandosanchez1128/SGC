718b4a0d1295774e89046c511f5aac4cd299fc42
juandprado <juandipra@hotmail.com>
2015-05-20 08:42:10 -0500

    Eliminada instancia de VistaLogin

--- SGC/Vista/mainwindow.py
+++ SGC/Vista/mainwindow.py
@@ -25,7 +25,6 @@ class MainWindow(QMainWindow):
         self.connect(self.ui.boton, SIGNAL("clicked()"), self.asignacion)
         self.connect(self.ui.lista_cursos, SIGNAL("itemDoubleClicked(QListWidgetItem*)"), self.cargarNotas)
         self.connect(self.ui.tableWidget, SIGNAL(("cellChanged(int,int)")), self.guardarNota)
-        venCrear =  VistaLogin().exec_()
         #self.empezar()
 
 
