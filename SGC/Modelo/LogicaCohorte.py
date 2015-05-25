6aaeb191860368d6cc4f981994938d498220099a
fernandosanchez1128 <fernandosanchez1128@gmail.com>
2015-05-24 23:56:22 -0500

    solucionado conflictos con la logica de Cohorte

--- SGC/Modelo/LogicaCohorte.py
+++ SGC/Modelo/LogicaCohorte.py
@@ -9,8 +9,9 @@ class LogicaCohorte():
     def __init__(self):
         print ("contructorc")
 
-    def agregarCohorte(self, cohorte):
-        self.session.add(cohorte)
+    def agregarCohorte(self, id_curso, ano, semestre):
+        coh = Cohorte(id_curso = id_curso, ano = ano, semestre = semestre)
+        self.session.add(coh)
         self.session.commit()
         self.session.close()
 
@@ -33,12 +34,26 @@ class LogicaCohorte():
 
 
     def ultimoCohorte(self, id_curso, ano, semestre):
-        cohorte = self.session.query(Cohorte).filter_by(id_curso=id_curso, ano=ano, semestre=semestre).all()
+        cohortes= self.session.query(Cohorte).filter_by(id_curso=id_curso, ano=ano, semestre=semestre).all()
         self.session.close()
-        return cohorte[len(cohorte) - 1].id_cohorte
+        if not cohortes == []:
+            return cohortes[- 1]
+        else:
+            return None
 
+    def cohorteN(self, id_curso, ano, semestre, N):
+        print "N", N
+        cohortes= self.session.query(Cohorte).filter_by(id_curso=id_curso, ano=ano, semestre=semestre).all()
+        self.session.close()
+        return cohortes[N]
 
-'''		
-log = LogicaCohorte()
-print log.ultimoCohorte(123,3,1)
+    def numCohortes(self, id_curso, ano, semestre):
+        cohortes= self.session.query(Cohorte).filter_by(id_curso=id_curso, ano=ano, semestre=semestre).count()
+        self.session.close()
+        return cohortes
+
+'''
+coh = Cohorte(id_curso = 4, ano = 2015, semestre = 2)
+lc= LogicaCohorte()
+lc.agregarCohorte(coh )
 '''
