7018a611caf5fde5150d7850d9efe6fff40d5a2b
Brayan Rodriguez <bryan951112@gmail.com>
2015-05-26 10:06:49 -0500

    avance notasEstudiante

--- SGC/Vista/NotasEstudiante.py
+++ SGC/Vista/NotasEstudiante.py
@@ -1,5 +1,6 @@
 from PyQt4 import uic
 from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 from PyQt4 import QtCore, QtGui
 
 from Control.ControlCoordinador import ControlCoordinador
@@ -11,37 +12,32 @@ class NotasEstudiante ( QDialog ):
     def __init__ ( self, parent = None ):
         QDialog.__init__( self, parent )
         self.ui = Ui_NotasEstudiante()
-        self.ui.setupUi( self )
+        self.ui.setupUi(self)
+        self.cedula_lt = None
         self.connect(self.ui.reporte, SIGNAL("clicked()"), self.generar_reporte)
         self.connect(self.ui.salir, SIGNAL("clicked()"), self.close)
 
     def __del__ ( self ):
         self.ui = None
 
-    def cargar_cursos (self, cedula_lt):
-        mats = self.controlcordinador.cursosEstudiantes(cedula_lt)
+    def cargar_cursos (self):
+        if self.cedula_lt!=None:
+            mats = self.controlcordinador.cursosEstudiantes(self.cedula_lt)
         cursos = []
         for mat in mats:
             cursos.append(self.controlcordinador.buscarCursoId(mat.id_curso))
         for curso in cursos :
             self.ui.cursos.addItem(QString(curso.nombre))
 
+
     def generar_reporte (self):
         curso = str (self.ui.cursos.currentText())
         obj_curso = self.controlcordinador.buscarCurso(curso)
-        if nmes == 2:
-            num_dias = 28
-        elif (nmes == 4 or nmes == 6 or nmes == 4 or nmes == 9 or nmes == 11):
-            num_dias =30
-        else:
-            num_dias =31
 
-        fecha_ini = date(year,nmes,1)
-        fecha_fin = date(year,nmes,num_dias)
         ruta = QFileDialog.getSaveFileName(self, 'Guardar Reporte', '', selectedFilter='*.pdf')
-        if ruta:
-            exito =self.controlcordinador.estudiantes_departamento(str (ruta),fecha_ini,fecha_fin,obj_curso.id,curso,mes,str (year))
+        if ruta and self.cedula_lt!=None:
+            exito =self.controlcordinador.estudiantes_departamento(str (ruta),self.cedula_lt, obj_curso)
             if exito == 0 :
                 QtGui.QMessageBox.warning(self, 'Error',"no se encontraron datos para generar reporte", QtGui.QMessageBox.Ok)
         else :
-            QtGui.QMessageBox.warning(self, 'Error',"por favor seleccione un destino", QtGui.QMessageBox.Ok)
+            QtGui.QMessageBox.warning(self, 'Error',"Por favor seleccione un destino y un lt", QtGui.QMessageBox.Ok)
